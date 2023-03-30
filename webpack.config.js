412aeb760e32ed4d82f201c756b11ce57dd6bdd6
Kai-Itakura <itakai199969@gmail.com>
2023-03-30 13:21:42 +0900

    強制的にコミットした

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
-const path = require('path');
+import path from 'path'
 
 module.exports = {
-    entry: {
-        bundle: './src/index.ts'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
-        },
-        open: true
-    },
-    module: {
-        rules: [
-            {
-                loader: 'ts-loader',
-                test: /\.ts$/
-            }
-        ]
-    }
+	entry: {
+		bundle: './src/index.ts'
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist')
+		},
+		open: true
+	},
+	module: {
+		rules: [
+			{
+				loader: 'ts-loader',
+				test: /\.ts$/
+			}
+		]
+	},
+	node: {
+		__dirname: true
+	}
 }
