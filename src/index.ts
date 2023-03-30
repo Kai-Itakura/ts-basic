7ec734d7e86232551fb5f39b84f3f0e6c4319c51
Kai-Itakura <itakai199969@gmail.com>
2023-03-30 11:31:59 +0900

    ESLintとPrettierの設定を追記

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import World from "./world";
+import World from './world'
 
 const root = document.getElementById('root')
-const world = new World('Hello World!');
-world.sayHello(root);
+const world = new World('Hello World!')
+
+world.sayHello(root)
