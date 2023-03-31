a169c9334a58868966b18b67191cace5ed34c6b3
Kai-Itakura <itakai199969@gmail.com>
2023-03-31 21:51:36 +0900

    型エイリアス(type)でオブジェクトの型定義をする

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,21 @@
 // unknownSample()
 
 /* 04. 関数の型定義 */
-import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
-logMessage('Hello TypeScript')
-logMessage2('Hello TypeScript')
-logMessage3('Hello TypeScript')
-logMessage4('Hello TypeScript')
-isUserSignedIn('ABC', 'KAI ITAKURA')
-isUserSignedIn('DEF')
-isUserSignedIn2('ABC')
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameter sample 5:', sum)
+// logMessage('Hello TypeScript')
+// logMessage2('Hello TypeScript')
+// logMessage3('Hello TypeScript')
+// logMessage4('Hello TypeScript')
+// isUserSignedIn('ABC', 'KAI ITAKURA')
+// isUserSignedIn('DEF')
+// isUserSignedIn2('ABC')
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('Function parameter sample 5:', sum)
+
+/* 05.オブジェクトの型定義 */
+import typeAliasSample from './object/alias'
+import objectSample from './object/object'
+objectSample()
+typeAliasSample()
